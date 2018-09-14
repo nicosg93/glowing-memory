3d0dae022e7bd642019c5cdba981a9d21df9c1f6
Nicolas Galvan <nicosg93@hotmail.com>
2018-09-14 15:01:53 -0300

    Agrego Modificacion ejercicio front end
    modifico el ejecicio 4, para que muestre json en un string

--- Ejercicios-Tenaris/Front_End/Ejercicio4/json.js
+++ Ejercicios-Tenaris/Front_End/Ejercicio4/json.js
@@ -1,3 +1,6 @@
+$( document ).ready(function() {
+
+
 var json =
 {
 	"moto":
@@ -29,6 +32,71 @@ var json =
 	]
 };
 
+
+
+var motos = [];
+
+//------------------------------------------------------------------------------------------------------
+var zanella = new Object();
+zanella.marca = "Zanella";
+zanella.modelo = [];
+
+var modeloZanellaZB110 = new Object();
+modeloZanellaZB110.nombre= "ZB110";
+modeloZanellaZB110.velocidadMax= 300;
+var modeloZanellaRX150 = new Object();
+modeloZanellaRX150.nombre = "RX150";
+modeloZanellaRX150.velocidadMax=250;
+
+zanella.modelo.push(modeloZanellaZB110);
+zanella.modelo.push(modeloZanellaRX150);
+
+//---------------------------------------------------------------------------------------------------------
+
+var honda = new Object();
+honda.marca = "Honda";
+honda.modelo= [];
+
+var modeloHondaWava110S = new Object();
+modeloHondaWava110S.nombre= "Wava 110 S";
+modeloHondaWava110S.velocidadMax = 325;
+var modeloHondaCG150Titan = new Object();
+modeloHondaCG150Titan.nombre = "CG150 Titan";
+modeloHondaCG150Titan.velocidadMax = 225;
+
+honda.modelo.push(modeloHondaWava110S);
+honda.modelo.push(modeloHondaCG150Titan);
+
+//----------------------------------------------------------------------------------------------------------
+
+var motomel = new Object();
+motomel.marca = "Motomel";
+motomel.modelo = [];
+
+var modeloMotomelB110 = new Object();
+modeloMotomelB110.nombre= "B110";
+modeloMotomelB110.velocidadMax = 275;
+var modeloMotomelCX150 = new Object();
+modeloMotomelCX150.nombre = "CX150";
+modeloMotomelCX150.velocidadMax = 175;
+
+motomel.modelo.push(modeloMotomelB110);
+motomel.modelo.push(modeloMotomelCX150);
+
+//----------------------------------------------------------------------------------------------------------
+
+motos.push(zanella);
+motos.push(honda);
+motos.push(motomel);
+
+
+var json1 = JSON.stringify(motos);
+
+ $('#json').append("El json se ve:");
+ $('#json').append(json1);
+
+//---------------------------------------------------------------------------------------------------------
+
 var d = '<tr>'+
 '<th>Marca</th>'+
 '<th>Modelos</th>'+
@@ -46,4 +114,7 @@ $(function () {
 	d+= '</tr>';
  }
  $("#tabla").append(d);
+
+});
+
 });
